154a5c2eabd1703f0340df5993a79d739ced4e58
ivelak <rantaana@gmail.com>
2018-02-08 15:56:08 +0200

    dev

---
+++ index.js
@@ -0,0 +1,88 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+let persons = [
+    {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": 1
+    },
+    {
+        "name": "Martti Tienari",
+        "number": "040-123456",
+        "id": 2
+    },
+    {
+        "name": "Arto Järvinen",
+        "number": "040-123456",
+        "id": 3
+    },
+    {
+        "name": "Lea Kutvonen",
+        "number": "040-123456",
+        "id": 4
+    }
+]
+
+app.use(bodyParser.json())
+
+
+
+app.get('/info', (req, res) => {
+    const size = persons.length
+    let now = new Date()
+    const front =`<p>puhelinluettelossa on ${size} henkilön tiedot</p>` + now
+    console.log('nyt',now)
+    res.send(front)
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const note = persons.find(note => note.id == id)
+    if (note) {
+        response.json(note)
+    }
+    else {
+        response.status(404).end()
+    }
+})
+
+const generateId = () => {
+    const maxId = persons.length > 0 ? persons.map(n => n.id).sort().reverse()[0] : 1
+    return maxId + 1
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (body.name === undefined) {
+        return response.status(400).json({ error: 'content missing' })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId()
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(note => note.id !== id)
+
+    response.status(204).end()
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
